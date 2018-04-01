0bcba83612b9f5b1a050a8c0e9cd42887ce71ceb
PotatoLovePotato <33536220+PotatoLovePotato@users.noreply.github.com>
2018-04-02 01:48:02 +0800

    BWS

--- xinxi/js/main.js
+++ xinxi/js/main.js
@@ -1,71 +1,12 @@
-
-$(function () { // 添加省略号
-    var clip = $('#bws_text_clip')
-    if(clip.text().length > 59){
-        clip.text(clip.text().substring(0, 80))
-        clip.html(clip.html() + '...')
-    }
-})
-
-$(function(){
-
-    moveElement({
-        ele: $('.js_move_ele').eq(0),
-        parent: $('.js_move_wrap').eq(0),
-        btns: [
-            $('.js_left_btn').eq(0),
-            $('.js_right_btn').eq(0)
-        ],
-        speed: 500
+$(function () {
+    $('.js_page').pagination({
+        mode: 'fixed',
+        current: 1, // 当前显示的页
+        totalData: 600, // 数据总条数
+        showData: 40, // 每页显示数据数
+        count: 8, // 分页器的显示数量 显示比设置的数多一个
+        callback: function (page) {
+            console.log(page.getCurrent())
+        }
     })
-    moveElement({
-        ele: $('.js_move_ele').eq(1),
-        parent: $('.js_move_wrap').eq(1),
-        btns: [
-            $('.js_left_btn').eq(1),
-            $('.js_right_btn').eq(1)
-        ],
-        speed: 500
-    })
-    function moveElement(eleObj) {
-        var ele = eleObj['ele'],
-            parent = eleObj['parent'],
-            btns = eleObj['btns'],
-            animate = eleObj['animate'],
-            speed = eleObj['speed']
-
-        var childrenWidth = ele.children().eq(0).outerWidth(true),
-            childrenLen = ele.children().length,
-            parentWidth = parent.width(),
-            maxMove = parseInt(((childrenWidth * childrenLen) - parentWidth) / childrenWidth),currentMove = 0
-        ele.css({
-            width: `${childrenWidth * childrenLen}px`
-        })
-        btns[0].click(function () {
-            console.log(1)
-            if (!ele.is(":animated")){
-                currentMove++
-                if (currentMove > maxMove) {
-                    currentMove = maxMove
-                }
-                ele.animate({
-                    left: `-${childrenWidth * currentMove}px`
-                }, speed)
-            }
-            
-        })
-        btns[1].click(function () {
-            console.log(2)            
-            if (!ele.is(":animated")){
-                currentMove--
-                if (currentMove < 0) {
-                    currentMove = 0
-                }
-                ele.animate({
-                    left: `-${childrenWidth * currentMove}px`
-                }, speed)
-            }
-        })
-    }
-
 })
